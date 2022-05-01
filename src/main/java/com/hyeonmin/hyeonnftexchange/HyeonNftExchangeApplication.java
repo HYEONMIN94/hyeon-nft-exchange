970be37983a385ef8bb7eceb7ea953215bd44832
JH <theking9494@naver.com>
2022-05-02 00:18:33 +0900

    오잉?

---
+++ src/main/java/com/hyeonmin/hyeonnftexchange/HyeonNftExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.hyeonmin.hyeonnftexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyeonNftExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyeonNftExchangeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/hyeonmin/hyeonnftexchange/controller/mainController.java
@@ -0,0 +1,17 @@
+package com.hyeonmin.hyeonnftexchange.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class mainController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
+
+
+        return "hello";
+    }
+}
